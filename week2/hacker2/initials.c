42e5e07ae6e45a49f266b0c35503199719c4e82c
Roy Wei <roywjy@gmail.com>
2016-12-24 22:34:12 +0800

    add initials.c

---
+++ week2/hacker2/initials.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string name = GetString();
+    
+    if ( name[0] != ' ' )
+    {
+        printf("%c", toupper(name[0]));
+    }
+    for ( int i = 0, n = strlen(name); i < n; i++ )
+    {
+        if ( name[i] == ' ' && name[i + 1] != '\0' ) 
+        {
+            printf("%c", toupper(name[i + 1])); 
+            i++;
+        }
+       
+    }
+    printf("\n"); 
+}
