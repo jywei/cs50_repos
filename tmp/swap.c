00513996bfe588633669df1ebc5b16899cb93b2a
Roy Wei <roywjy@gmail.com>
2017-01-22 18:13:26 +0800

    modified

--- tmp/swap.c
+++ tmp/swap.c
@@ -1,30 +1,33 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-void swap(char *c1, char *c2){
+void swap(char *c1, char *c2)
+{
     *c1 ^= *c2 ^= *c1 ^= *c2;
 }
 
-/* 想法 */
-char *reverse_iterative(char *s){
+char *reverse_iterative(char *s)
+{
     int len = strlen(s);
     int i;
-    for ( i = 0; i < len / 2; i++){
+    for ( i = 0; i < len / 2; i++)
+    {
         swap(s + i, s + (len - 1 - i));
     }
     return s;
 }
 
-
-
-char *reverse_recursive(char *s){
+char *reverse_recursive(char *s)
+{
     static int time = 0;
     static int len = 0;
 
-    if (time == 0){
+    if (time == 0)
+    {
         len = strlen(s);
     }
-    if (time == len / 2){
+    if (time == len / 2)
+    {
         return s;
     }
 
@@ -37,7 +40,6 @@ char *reverse_recursive(char *s){
 
 int main(){
     char str[10] = "hello";
-//  printf("%s\n", reverse_iterative(str));
     reverse_recursive(str);
     printf("%s\n",  reverse_recursive(str));
     return 0;
