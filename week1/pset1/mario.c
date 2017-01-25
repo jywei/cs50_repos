ac655894e727d499b3504a79d6d17a0b150247da
Roy Wei <roywjy@gmail.com>
2017-01-25 12:59:51 +0800

    mario improved

--- week1/pset1/mario.c
+++ week1/pset1/mario.c
@@ -4,26 +4,25 @@
 
 int main(void)
 {
-    printf("height of the pyramid (smaller than 23): ");
-    int height = get_int();
-    if (height > 23 || height < 1)
+    int height = 0;
+
+    do
     {
-        printf("height of the pyramid (smaller than 23 and greater than 1), idiot: ");
-    }
-    else
+        printf("height of the pyramid (smaller than 23 and greater than 1): \n");
+        height = get_int();
+    } while (height > 23 || height < 1);
+
+    for (int i = 0; i < height; i++)
     {
-        for (int i = 0; i < height; i++)
+        for (int j = 0; j < (height - i - 1); j++)
         {
-            for (int j = 0; j < (height - i -1); j++)
-            {
-                printf("%s", " ");
-            }
-            for (int k = 0; k < (i + 2); k++)
-            {
-                printf("%s", "#");
-            }
-            printf("\n");
+            printf("%s", " ");
         }
+        for (int k = 0; k < (i + 2); k++)
+        {
+            printf("%s", "#");
+        }
+        printf("\n");
     }
     return 0;
 }
