e76b196b960d0241af69cb2dbb7a6fb580fe9830
Roy Wei <roywjy@gmail.com>
2017-04-06 23:46:19 +0800

    Add initials w/o space check

---
+++ week2/pset2/initials.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  string name = GetString();
+
+  printf("%c", toupper(name[0]));
+
+  for (int i = 0; i < strlen(name); i++)
+  {
+    if (name[i] == ' ' && name[i + 1] != '\0')
+    {
+      printf("%c", toupper(name[i + 1]));
+      i++;
+    }
+  }
+  printf("\n");
+}
