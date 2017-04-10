33381d31df2f88fce21a4e4bf102408a79e053fb
Roy Wei <roywjy@gmail.com>
2017-04-10 23:50:13 +0800

    Apply Caecar cipher

---
+++ week2/pset2/caesar.c
@@ -0,0 +1,56 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Get the key to the cipher
+int main(int argc, string argv[])
+{
+    string key = argv[1];
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar k\n");
+        return 1;
+    }
+    else
+    {
+        // Convert the string to an integer
+        int k = atoi(key) % 26;
+        // Check that it is a valid key
+        if (k == 0)
+        {
+            printf("Usage: ./caesar k\n");
+            return 1;
+        }
+        // Get the text to encrypt
+        printf("plaintext:  ");
+        string plainText = GetString();
+        if (plainText != NULL)
+        {
+            // Encrypt and print
+            for (int i = 0, n = strlen(plainText); i < n; i++)
+            {
+                int c = 0;
+                // Check if text is upper case or lower case
+                // If so, switch to alphabet number and encrypt
+                if (isupper(plainText[i]))
+                {
+                    c = ((plainText[i] - 65 + k) % 26) + 65;
+                    printf("ciphertext: %c", c);
+                }
+                else if (islower(plainText[i]))
+                {
+                    c = ((plainText[i] - 97 + k) % 26) + 97;
+                    printf("ciphertext: %c", c);
+                }
+                else
+                {
+                    printf("plaintext: %c", plainText[i]);
+                }
+            }
+            printf("\n");
+            return 0;
+        }
+    }
+}
