f0834a832694f3682f60222d2e125a92150ba680
Roy Wei <roywjy@gmail.com>
2017-06-10 15:10:53 +0800

    Reapply week2 in python

--- week8/src8/week2/argv0.py
+++ week8/src8/week2/argv0.py
@@ -4,3 +4,8 @@ if len(sys.argv) == 2:
     print("hello, {}".format(sys.argv[1]))
 else:
     print("hello, world")
+
+if len(sys.argv) == 2:
+    print("hello, " + sys.argv[1])
+else:
+    print("hello, world")
